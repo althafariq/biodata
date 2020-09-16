254c74554fe8f244b3bca332c0cfb929c599c8ab
althafariq <althafariq04@gmail.com>
2020-09-16 12:02:58 +0700

    biodata

---
+++ biodata.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("Nama : Althaf Ariq M Ghufron\n");
+	printf("TTL : Makassar, 4 Maret 2000\n");
+	printf("NPM : 19081010102\n");
+	printf("Jurusan / Angkatan : T.Informatika / 2019");
+	return 0;
+}
